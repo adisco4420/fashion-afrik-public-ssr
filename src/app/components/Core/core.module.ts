115e7f1f596073a09ca5d60435630dc5b65c2b7e
alabi sodiq <adisco4420@gmail.com>
2018-10-08 23:56:29 +0100

    forget password design

--- src/app/components/Core/core.module.ts
+++ src/app/components/Core/core.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { LandingComponent } from './landing/landing.component';
 import { FooterComponent } from './footer/footer.component';
+
 import { SharedModule } from '../../shared/shared.module';
 import { AppRoutingModule } from '../../app-routing.module';
 import { CommonModule } from '@angular/common';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 @NgModule({
     declarations: [LandingComponent,
     FooterComponent, NotFoundPageComponent],
     exports: [ LandingComponent, FooterComponent, NotFoundPageComponent, AppRoutingModule, CommonModule],
-    imports: [SharedModule, AppRoutingModule, CommonModule]
+    imports: [
+        SharedModule,
+        AppRoutingModule,
+        CommonModule,
+        HttpClientJsonpModule,
+        HttpClientModule
+    ]
 })
 
 
